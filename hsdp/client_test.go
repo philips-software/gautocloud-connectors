7508c8cd63dfd09c8255f8a47be136de36ce61ab
Andy Lo-A-Foe <andy.loafoe@gmail.com>
2018-10-26 15:58:15 +0200

    Add client test

---
+++ hsdp/client_test.go
@@ -0,0 +1,42 @@
+package hsdp_test
+
+import (
+	"github.com/cloudfoundry-community/gautocloud/connectors"
+	. "github.com/loafoe/gautocloud-connectors/hsdp"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Client", func() {
+	var connector connectors.Connector
+	Context("Twilio", func() {
+		BeforeEach(func() {
+			connector = NewTwilioClientConnector()
+		})
+		It("Should return a Twilio Go client when passing a TwilioSchema", func() {
+			data, err := connector.Load(TwilioSchema{
+				TwilioAuthToken: "StrongPassw0rd",
+				TwilioSID:       "AKFooBar",
+			})
+			Expect(err).NotTo(HaveOccurred())
+			Expect(data).NotTo(BeNil())
+		})
+	})
+	Context("DynamoDB", func() {
+		BeforeEach(func() {
+			connector = NewDynamoDBClientConnector()
+		})
+		It("Should return a DynamoDB Go client when passing a DynamoDBSchema", func() {
+			data, err := connector.Load(DynamoDBSchema{
+				AWSKey:    "some-key",
+				AWSRegion: "us-east-1",
+				AWSSecret: "StrongPassw0rd",
+				TableName: "table-name",
+			})
+			Expect(err).NotTo(HaveOccurred())
+			Expect(data).NotTo(BeNil())
+		})
+	})
+
+})
